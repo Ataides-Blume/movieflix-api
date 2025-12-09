122d1b16607437c178bda85004cee23475d19866
Ataides Blume <abo.revelacao@outlook.com>
2025-12-09 20:18:36 -0300

    ajustei conf.prisma, atualizado Genre conf. documentação, corrigi arquivos server.ts e adicionei rota de filmes

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,25 @@
-import type { Request, Response } from 'express'
-import express = require('express')
+import "dotenv/config";
+import type { Request, Response } from "express"
+import express from "express"
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from './generated/prisma/client'
 
 const port = 3000
 const app = express()
 
-app.get('/movies', (req: Request, res: Response) => {
-  res.send('Listagem de filmes')
+const connectionString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+export { prisma }
+
+app.get("/movies", async (req: Request, res: Response) => {
+    const movies = await prisma.movie.findMany()
+    res.json(movies)
 })
 
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`)
 })
 
-/*const http = require('http')
-
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/plain')
-
-    if (req.url === '/') {
-        res.statusCode = 200
-        res.end('Home page Blume')
-    } else if (req.url === '/sobre') {
-        res.statusCode = 200
-        res.end('About page')
-    }
-})
-
-server.listen(3000, () => {
-    console.log(`Servidor em execução em http://localhost:3000/`)
-})*/
