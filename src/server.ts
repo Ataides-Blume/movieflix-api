44d5a17a33e0217c0e2458fdeb941f0c9860b570
Ataides Blume <abo.revelacao@outlook.com>
2025-12-12 23:07:36 -0300

    adicionada mensagem de erro no na criação do filme

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import "dotenv/config";
+import "dotenv/config"
 import type { Request, Response } from "express"
 import express from "express"
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from './generated/prisma/client'
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "./generated/prisma/client"
 
 const port = 3000
 const app = express()
@@ -12,22 +12,41 @@ const connectionString = `${process.env.DATABASE_URL}`
 const adapter = new PrismaPg({ connectionString })
 const prisma = new PrismaClient({ adapter })
 
+app.use(express.json())
+
 export { prisma }
 
 app.get("/movies", async (req: Request, res: Response) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
-            title: 'asc'
+            title: "asc",
         },
-        include:{
+        include: {
             genres: true,
             languages: true,
-        }
+        },
     })
     res.json(movies)
+})
+
+app.post("/movies", async (req: Request, res: Response) => {
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body
+    try {
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            },
+        })
+    } catch (err) {
+        return res.status(500).send({ message: "Erro ao cadastrar filme" })
+    }
+    res.status(201).send()
 })
 
 app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`)
 })
-
